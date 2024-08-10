d95093dc60466fefee6a4ee653b6afed4e8c8983
Akbar014 <akbar.cse47@gmail.com>
2024-08-10 16:27:27 +0600

    First Commit

---
+++ donate_blood/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import UserAccount, DonationRequest, DonatioHistory, DonationAccepted
+# Register your models here.
+admin.site.register(UserAccount),
+admin.site.register(DonationRequest),
+admin.site.register(DonatioHistory),
+admin.site.register(DonationAccepted),
